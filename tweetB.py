2af1b9bd4af3f05a280651aa88819363f328ec7e
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-01-27 18:25:26 +0200

    done

--- tweetB.py
+++ tweetB.py
@@ -1,4 +1,20 @@
 import tweepy
 import keys
 def api():
-    auth = tweepy.OAuth5 
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
+    auth.set_access_token(keys.access, keys.access_secret)
+
+    return tweepy.API(auth)
+
+def tweet(api: tweepy.API, message:str, image_path=None):
+    if image_path:
+        api.update_status_with_media(message, image_path)
+    else:
+        api.update_status(message)
+
+    print('Done')
+
+if __name__ == '__name__':
+    api = api()
+    tweet(api, 'This was tweeted by a bot!', 'photos\light1.png')
+
