07916982d8e2d9820da13a8edafbfa7f6a2976ed
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-01 00:07:20 +0200

    Small fixes

--- quotesofthedayB.py
+++ quotesofthedayB.py
@@ -1,19 +1,32 @@
 import keys
 import requests
 import tweepy
+import schedule
+import time 
 
-auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
-auth.set_access_token(keys.access, keys.access_secret)
+def quote():
 
-response = requests.get("https://quotes.rest/qod")
-data = response.json()
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
+    auth.set_access_token(keys.access, keys.access_secret)
 
-quote = data['contents']['quotes'][0]['quote']
-author = data['contents']['quotes'][0]['author']
+    response = requests.get("https://quotes.rest/qod")  
+    data = response.json()
 
-print("Quote: " + quote)
-print("Author: " + author)
+    quote = data['contents']['quotes'][0]['quote']
+    author = data['contents']['quotes'][0]['author']
 
-api = tweepy.API(auth)
-quoteD = "Quote: " + quote + "\n" + "Author: " + author
-api.update_status(quoteD)
+    print("Quote: " + quote)
+    print("Author: " + author)
+
+    api = tweepy.API(auth)
+    quoteD = "Quote: " + quote + "\n" + "Author: " + author
+    api.update_status(quoteD)
+
+
+schedule.every().hours.do(quote)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
--- tweetB.py
+++ tweetB.py
@@ -50,3 +50,4 @@ while True:
 
 
 
+ 
